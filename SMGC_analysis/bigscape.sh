3930d256b05522120466bbb3d4ac3ba86013cb3a
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2022-04-20 16:07:03 +0100

    Create bigscape.sh

---
+++ SMGC_analysis/bigscape.sh
@@ -0,0 +1,20 @@
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=32:mem=124gb
+
+module load anaconda3/personal
+source activate bigscape
+
+cd $PBS_O_WORKDIR
+
+python bigscape.py \
+ -i antismash_Leca45T_output/antismash_Leca45T_gbks \
+ -o BigScape_c0.46_clansoff_Leca45T_round2_output \
+ -c 32 \
+ --cutoffs 0.4 0.5 0.6 0.7 0.8 0.9 1.0 \ #here add whichever cut-offs you want
+ --banned_classes RiPPs Saccharides \
+ --hybrids-off \
+ --clans-off \
+ -v \
+ --mibig
+ 
+ conda deactivate
