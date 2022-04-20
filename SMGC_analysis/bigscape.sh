55b13e68819809d63c0ab6514db20f9b9c1b3ad4
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2022-04-20 16:09:53 +0100

    Update bigscape.sh

--- SMGC_analysis/bigscape.sh
+++ SMGC_analysis/bigscape.sh
@@ -8,7 +8,7 @@ cd $PBS_O_WORKDIR
 
 python bigscape.py \
  -i antismash_Leca45T_output/antismash_Leca45T_gbks \
- -o BigScape_c0.46_clansoff_Leca45T_round2_output \
+ -o BigScape_c0.4_1_clansoff_Leca45T_output \
  -c 32 \
  --cutoffs 0.4 0.5 0.6 0.7 0.8 0.9 1.0 \ #here add whichever cut-offs you want
  --banned_classes RiPPs Saccharides \
