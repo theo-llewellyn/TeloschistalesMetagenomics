e94e9740435916053dd5056154d152adfa2a5a2a
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2022-04-20 13:38:14 +0100

    Update extract_75_orthogroups.sh

--- phylogenomics/extract_75_orthogroups.sh
+++ phylogenomics/extract_75_orthogroups.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
 mkdir Single_Copy_Orthologues_75percent
 
-cat Orthologues_Leca37T_75p.txt | while read line; do cp Orthogroup_Sequences/${line}.fa Single_Copy_Orthologues_75percent; done
+cat Orthologues_Leca45T_75p.txt | while read line; do cp Orthogroup_Sequences/${line}.fa Single_Copy_Orthologues_75percent; done
 
