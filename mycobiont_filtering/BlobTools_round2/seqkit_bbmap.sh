48dfa23c96ca8a341281a9feb5428a713757cd2c
theo-llewellyn <51782438+theo-llewellyn@users.noreply.github.com>
2022-04-20 13:06:13 +0100

    Create seqkit_bbmap.sh

---
+++ mycobiont_filtering/BlobTools_round2/seqkit_bbmap.sh
@@ -0,0 +1,23 @@
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=16:mem=124gb
+
+module load anaconda3/personal
+source activate seqkit-env
+
+cd $PBS_O_WORKDIR
+
+#opens the assembly, searches for particular contigs and puts into a new file
+cat ASSEMBLY.fasta | seqkit grep -f ACCESSION_Asco_headers.fa > ACCESSION_Asco_contigs.fa
+
+conda deactivate
+
+#pull reads mapping to those contigs with bbmap
+module load bbmap
+
+bbmap.sh -Xmx62g \
+nodisk=t \
+ref=ACCESSION_Asco_contigs.fa \
+in1=ACCESSION_Trimmed_1P.fq.gz \
+in2=ACCESSION_Trimmed_2P.fq.gz \
+threads=16 \
+outm=ACCESSION_concoct_Asco_R#.fq.gz
